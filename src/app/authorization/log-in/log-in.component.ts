30da526b3b7ad4234a34910ab2d80f3491559846
BohdanDziadyk <bohdan.reaper669@gmail.com>
2020-12-23 18:48:54 +0200

    all crud comment completed

--- src/app/authorization/log-in/log-in.component.ts
+++ src/app/authorization/log-in/log-in.component.ts
@@ -1,10 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {LogInPair} from "../models/LogInPair";
-import {TokensPair} from "../models/TokensPair";
 import {AuthorizationService} from "../services/authorization.service";
 import {Router} from "@angular/router";
-import {query} from "@angular/animations";
 
 @Component({
   selector: 'app-log-in',
@@ -15,16 +12,13 @@ export class LogInComponent implements OnInit {
   form: FormGroup;
   username: FormControl = new FormControl('', [Validators.required]);
   password: FormControl = new FormControl('', [Validators.required, Validators.minLength(8), Validators.maxLength(16)]);
-  // confirmPassword: FormControl = new FormControl('', [Validators.required, Validators.minLength(8), Validators.maxLength(16)]);
   error: any;
 
-  constructor(private authorizationService:AuthorizationService, private router: Router) {
+  constructor(private authorizationService: AuthorizationService, private router: Router) {
     this.form = new FormGroup({
       username: this.username,
       password: this.password,
-      // confirmPassword: this.confirmPassword
     });
-    // , this.passwordValidator.bind(this));
   }
 
   logIn(form: FormGroup): void {
@@ -33,12 +27,6 @@ export class LogInComponent implements OnInit {
     }, err => this.error = err.detail)
   }
 
-
-  // passwordValidator(form: FormGroup): null | object {
-  //   const {value : password} = form.controls.password;
-  //   const {value: confirmPassword} = form.controls.confirmPassword;
-  //   return password === confirmPassword ? null : {passwordError: true};
-  // }
   ngOnInit(): void {
   }
 
--- src/app/post/post/post.component.ts
+++ src/app/post/post/post.component.ts
@@ -7,6 +7,7 @@ import {Comment} from "../../comment/models/Comment";
 import {UserService} from "../../user/services/user.service";
 import {User} from "../../user/models/User";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {UserAccountService} from "../../user-account/services/user-account.service";
 
 @Component({
   selector: 'app-post',
@@ -19,30 +20,52 @@ export class PostComponent implements OnInit {
   commentUser: User;
   post: Post;
   comments: Comment[];
-  newComment: Comment;
+  commentEditFlag = false;
+  comment: Comment;
   form: FormGroup;
   body: FormControl = new FormControl('', [Validators.required]);
+  commentEditForm: FormGroup;
+  editedCommentBody: FormControl = new FormControl('', [Validators.required]);
 
   constructor(private activatedRoute: ActivatedRoute, private userService: UserService, private postService: PostService,
-              private commentService: CommentService) {
+              private commentService: CommentService, private userAccountService: UserAccountService) {
     this.activatedRoute.params.subscribe(value => this.postService.getPost(value.id).subscribe(value1 => this.post = value1));
     this.activatedRoute.params.subscribe(value => this.commentService.getCommentByPostId(value.id)
       .subscribe(value1 => this.comments = value1));
     this.form = new FormGroup({
       body: this.body
     })
+    this.commentEditForm = new FormGroup({
+      editedCommentBody: this.editedCommentBody
+      }
+    )
   }
 
   doComment(form: FormGroup) {
     this.postService.doComment({body: form.controls.body.value, post: this.post.id}).subscribe();
     document.location.reload();
   }
+  showCommentEditForm(): boolean{
+    return this.commentEditFlag;
+  }
+  changeCommentEditFlag(comment):void{
+    this.comment = comment;
+    this.commentEditFlag = !this.commentEditFlag;
+  }
+  editComment(commentEditForm:FormGroup){
+    this.userAccountService.editComment({body: commentEditForm.controls.editedCommentBody.value, post: this.post.id}, this.comment.id).subscribe()
+    document.location.reload()
+  }
+
+  deleteComment(id){
+    this.userAccountService.deleteComment(id).subscribe()
+    document.location.reload()
+  }
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(value => this.postService.getPost(value.id)
       .subscribe(value1 => this.userService.getUser(value1.user).subscribe(value2 => this.postUser = value2)));
-    this.activatedRoute.params.subscribe(value => this.commentService.getComment(value.id)
-      .subscribe(value1 => this.userService.getUser(value1.user).subscribe(value2 => this.commentUser = value2)));
+    this.userAccountService.getCurrentUser().subscribe(value => this.commentUser = value);
   }
 
 }
